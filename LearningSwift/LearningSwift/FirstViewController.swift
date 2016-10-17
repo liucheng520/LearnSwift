5baacac94835f4922f70b9af1a165ed3c7c3ab74
刘成 <209207878@qq.com>
2016-10-17 18:08:05 +0800

    定位

--- LearningSwift/LearningSwift/FirstViewController.swift
+++ LearningSwift/LearningSwift/FirstViewController.swift
@@ -12,7 +12,7 @@ class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataS
 
     var tableView : UITableView!
 
-    var dataSource : NSArray = ["timeLearning","fontLearning","localVedioLearning","imageBrowser","moveTest"]
+    var dataSource : NSArray = ["timeLearning","fontLearning","localVedioLearning","imageBrowser","moveTest","LocationViewController"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,6 +57,10 @@ class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataS
             navigationController?.pushViewController(viewCtrol, animated: true)
         }else if indexPath.row == 4 {
             let viewCtrol = MoveTestViewController.init()
+            viewCtrol.title = dataSource[indexPath.row] as? String
+            navigationController?.pushViewController(viewCtrol, animated: true)
+        }else if indexPath.row == 5 {
+            let viewCtrol = LocationViewController.init()
             viewCtrol.title = dataSource[indexPath.row] as? String
             navigationController?.pushViewController(viewCtrol, animated: true)
         }
---
+++ LearningSwift/LearningSwift/LocationViewController.swift
@@ -0,0 +1,77 @@
+//
+//  LocationViewController.swift
+//  LearningSwift
+//
+//  Created by 刘成 on 2016/10/17.
+//  Copyright © 2016年 fff. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationViewController: UIViewController,CLLocationManagerDelegate {
+
+    var placeLabel : UILabel!
+    
+    var locationManger : CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+        
+        placeLabel = UILabel.init()
+        placeLabel.frame = CGRect.init(x: 0, y: 70, width: kScreenWidth, height: 30);
+        placeLabel.textAlignment = NSTextAlignment.center
+        placeLabel.textColor = UIColor.white
+        placeLabel.text = "MY Location"
+        view.addSubview(placeLabel)
+        
+        let findButton : UIButton = UIButton.init(type: UIButtonType.custom)
+        findButton.setTitle("Find my location", for: UIControlState.normal)
+        findButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        findButton.frame = CGRect.init(x: 50, y: 300, width: kScreenWidth - 100, height: 50)
+        view.addSubview(findButton)
+        findButton.addTarget(self, action: #selector(self.startLocation), for: UIControlEvents.touchUpInside)
+    }
+
+    //获取定位权限，开始定位
+    func startLocation() -> Void {
+        locationManger = CLLocationManager.init()
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.requestAlwaysAuthorization()
+        locationManger.startUpdatingLocation()
+    }
+    
+    //获取定位结果，并转码
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        CLGeocoder.init().reverseGeocodeLocation(manager.location!) { (placeMarks, error) in
+            if error != nil {
+                self.placeLabel.text = "Reverse geocoder failed with error" + error!.localizedDescription
+                return
+            }else{
+                if (placeMarks?.count)! > 0 {
+                    let pm : CLPlacemark = (placeMarks?[0])!
+                    self.showLocation(placeMark: pm)
+                }else{
+                    self.placeLabel.text = "Problem with the data received from geocoder"
+                }
+            }
+        }
+    }
+    
+    func showLocation(placeMark:CLPlacemark) -> Void {
+        
+        locationManger.stopUpdatingLocation()
+        
+        let locality = (placeMark.locality != nil) ? placeMark.locality : ""
+        let postalCode = (placeMark.postalCode != nil) ? placeMark.postalCode : ""
+        let administrativeArea = (placeMark.administrativeArea != nil) ? placeMark.administrativeArea : ""
+        let country = (placeMark.country != nil) ? placeMark.country : ""
+ 
+        placeLabel.text = String.init(format: "%@%@%@%@",locality!,postalCode!,administrativeArea!,country!)
+        
+    }
+}
