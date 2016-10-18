471e2813b012b6f65a88a081377228931a377e95
刘成 <209207878@qq.com>
2016-10-18 11:55:20 +0800

    视频采集完结

--- LearningSwift/LearningSwift/CenterViewController.swift
+++ LearningSwift/LearningSwift/CenterViewController.swift
@@ -39,14 +39,84 @@ class CenterViewController: UIViewController,AVCapturePhotoCaptureDelegate {
     var previewLayer : AVCaptureVideoPreviewLayer?
     var tempImageView : UIImageView?
     var didTakePhoto : Bool = false
+    var frontInput : AVCaptureDeviceInput?
+    var backInput : AVCaptureDeviceInput?
+    var isFront : Bool!
+    var errorFront : NSError?
+    var errorBack : NSError?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.green
         tempImageView = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: kScreenWidth, height: kScreenHeight))
         view.addSubview(tempImageView!);
+        
+        isFront = true
+        
+        let button : UIButton = UIButton.init(type: UIButtonType.custom)
+        button.setTitle("change", for: UIControlState.normal)
+        button.setTitleColor(UIColor.black, for: UIControlState.normal)
+        button.frame = CGRect.init(x: kScreenWidth - 60, y: 0, width: 50, height: 50)
+        view .addSubview(button)
+        button.addTarget(self, action: #selector(self.changeCamer), for: UIControlEvents.touchUpInside)
+        
+        //配置前置摄像头
+        let frontCamera = AVCaptureDevice.defaultDevice(withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.front)
+
+        do {
+            frontInput = try AVCaptureDeviceInput.init(device: frontCamera)
+        } catch let error1 as NSError {
+            errorFront = error1
+            frontInput = nil
+        }
+        
+        //配置后置摄像头
+        let backCamera = AVCaptureDevice.defaultDevice(withDeviceType: AVCaptureDeviceType.builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.back)
+        
+        do {
+            backInput = try AVCaptureDeviceInput.init(device: backCamera)
+        } catch let error2 as NSError {
+            errorBack = error2
+            backInput = nil
+        }
+        
     }
 
+    //前后摄像头切换
+    func changeCamer() -> Bool {
+        
+        if isFront == true {
+            isFront = false
+            if captureSession?.inputs.count == 0 {
+                if errorFront == nil && captureSession?.canAddInput(frontInput) != nil {
+                    captureSession?.addInput(frontInput)
+                    return true
+                }else{
+                    return false
+                }
+            }else{
+                captureSession?.removeInput(backInput)
+                if errorFront == nil && captureSession?.canAddInput(frontInput) != nil {
+                    captureSession?.addInput(frontInput)
+                    return true
+                }else{
+                    return false
+                }
+            }
+
+        }else{
+            isFront = true
+            captureSession?.removeInput(frontInput)
+            if errorBack == nil && captureSession?.canAddInput(backInput) != nil {
+                captureSession?.addInput(backInput)
+                return true
+            }else{
+                return false
+            }
+        }
+
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         previewLayer?.frame = view.bounds
@@ -57,31 +127,31 @@ class CenterViewController: UIViewController,AVCapturePhotoCaptureDelegate {
         
         super.viewWillAppear(animated)
         
+        //1 建立Session
         captureSession = AVCaptureSession.init()
-        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
         
-        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-        var error : NSError?
-        var input : AVCaptureDeviceInput?
+        //设置采集的质量
+        captureSession?.sessionPreset = AVCaptureSessionPreset640x480
         
-        do {
-             input = try AVCaptureDeviceInput.init(device: backCamera)
-        } catch let error1 as NSError {
-            error = error1
-            input = nil
-        }
+        //2 添加 input
+        let isadd = changeCamer()
         
-        if error == nil && captureSession?.canAddInput(input) != nil {
-            captureSession?.addInput(input)
-            stillImageOutput = AVCapturePhotoOutput.init()
+        if isadd == true {
             
+            //3 添加output
+            stillImageOutput = AVCapturePhotoOutput.init()
+            stillImageOutput?.photoSettingsForSceneMonitoring = AVCapturePhotoSettings.init(format: [AVVideoCodecKey : AVVideoCodecJPEG])
+      
             if captureSession?.canAddOutput(stillImageOutput) != nil {
-                captureSession?.canAddOutput(stillImageOutput)
+                captureSession?.addOutput(stillImageOutput)
                 
+                //添加页面显示
                 previewLayer = AVCaptureVideoPreviewLayer.init(session: captureSession)
                 previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
                 previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
                 view.layer.addSublayer(previewLayer!)
+                
+                //4 开始捕捉
                 captureSession?.startRunning()
             }
         }
@@ -106,14 +176,19 @@ class CenterViewController: UIViewController,AVCapturePhotoCaptureDelegate {
         let videoConnection : AVCaptureConnection = (stillImageOutput?.connection(withMediaType: AVMediaTypeVideo))!
         
         videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+        
+        //静态图片采集
         stillImageOutput?.capturePhoto(with: AVCapturePhotoSettings.init(), delegate: self)
     }
     
     func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
         if photoSampleBuffer != nil {
-            let imageData = AVCapturePhotoOutput.dngPhotoDataRepresentation(forRawSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+            let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+            
             let dataProvider = CGDataProvider.init(data: imageData as! CFData)
-            let cgImageRef = CGImage.init(pngDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+            let cgImageRef = CGImage.init(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
+            
             let image = UIImage.init(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
             
             tempImageView?.image = image
